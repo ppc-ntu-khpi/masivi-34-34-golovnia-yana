372274ce3d9787c6dc6f6eaa13f86a2be4c23196
Golovnia Yana <55803221+34-golovnia-yana@users.noreply.github.com>
2020-05-03 21:38:38 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,56 @@
+import java.util.Arrays;
 
 /**
  * The class in which the matrix is ‚Äã‚Äãtransposed
+ * 
  * @author Asus
  *
  */
-	public class Main {
-		
-	    public static void main(String[] args) {
-	        int[][] sourceMatrix = new int[3][4];
-	        int[][] transposedMatrix = new int[4][3];
+public class Main {
 
-	        sourceMatrixWithFiller(sourceMatrix);
-	        transposedMatrix(sourceMatrix, transposedMatrix);
-	    }
-/**
- * Method for initial matrix
- * @param sourceMatrix
- * @return sourceMatrix
- */
-	    public static int[][] sourceMatrixWithFiller(int[][] sourceMatrix) {
-	        int numberFiller = 1;
-	        System.out.println("–í–∏—Ö—ñ–¥–Ω–∞ –º–∞—Ç—Ä–∏—Ü—è:");
-	        for (int i = 0; i < sourceMatrix.length; i++) {
-	            for (int j = 0; j < sourceMatrix[i].length; j++) {
-	                sourceMatrix[i][j] = numberFiller++;
-	                System.out.print(sourceMatrix[i][j] + " ");
-	            }
-	            System.out.println();
-	        }
-	        return sourceMatrix; //return the result  
-	    }
-/**
- * Method for transposing and displaying a new matrix
- * @param sourceMatrix
- * @param transposedMatrix
- * @return transposedMatrix
- */
-	    public static int[][] transposedMatrix(int[][] sourceMatrix, int[][] transposedMatrix) {
-	        for (int i = 0; i < sourceMatrix.length; i++) {
-	            for (int j = 0; j < sourceMatrix[i].length; j++) {
-	                transposedMatrix[j][i] = sourceMatrix[i][j];
-	            }
-	        }
-	        System.out.println("–¢—Ä–∞–Ω—Å–ø–æ–Ω–æ–≤–∞–Ω–∞ –º–∞—Ç—Ä–∏—Ü—è:");
-	        for (int i = 0; i < transposedMatrix.length; i++) {
-	            for (int j = 0; j < transposedMatrix[i].length; j++) {
-	                System.out.print(transposedMatrix[i][j] + " ");
-	            }
-	            System.out.println();
-	        }
-	        return transposedMatrix; //return the result  
-	    }
+	public static void main(String[] args) {
+		int[][] sourceMatrix = new int[3][4];
+		int[][] transposedMatrix = new int[4][3];
+
+		sourceMatrixWithFiller(sourceMatrix);
+		transposedMatrix(sourceMatrix, transposedMatrix);
 	}
 
+	/**
+	 * Method for initial matrix
+	 * 
+	 * @param sourceMatrix
+	 * @return sourceMatrix
+	 */
+	public static int[][] sourceMatrixWithFiller(int[][] sourceMatrix) {
+		int numberFiller = 1;
+		System.out.println("œÓ˜‡ÚÍÓ‚‡ Ï‡ÚËˆˇ:");
+		for (int i = 0; i < sourceMatrix.length; i++) {
+			for (int j = 0; j < sourceMatrix[i].length; j++) {
+				sourceMatrix[i][j] = numberFiller++;
+			}
+		}
+		for (int[] row : sourceMatrix)
+			System.out.println(Arrays.toString(row));
+		return sourceMatrix; // return the result
+	}
+
+	/**
+	 * Method for transposing and displaying a new matrix
+	 * 
+	 * @param sourceMatrix
+	 * @param transposedMatrix
+	 * @return transposedMatrix
+	 */
+	public static int[][] transposedMatrix(int[][] sourceMatrix, int[][] transposedMatrix) {
+		for (int i = 0; i < sourceMatrix.length; i++) {
+			for (int j = 0; j < sourceMatrix[i].length; j++) {
+				transposedMatrix[j][i] = sourceMatrix[i][j];
+			}
+		}
+		System.out.println("“‡ÌÒÔÓÌÓ‚‡Ì‡ Ï‡ÚËˆˇ:");
+		for (int[] row : transposedMatrix)
+			System.out.println(Arrays.toString(row));
+		return transposedMatrix; // return the result
+	}
+}
