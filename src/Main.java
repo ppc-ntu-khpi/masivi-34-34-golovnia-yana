660c17f2a0697a487e9b6afddd519d9d6a2024e1
Golovnia Yana <55803221+34-golovnia-yana@users.noreply.github.com>
2020-05-03 19:45:39 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
-import java.util.Arrays;
 
-public class Main {
+/**
+ * The class in which the matrix is ​​transposed
+ * @author Asus
+ *
+ */
+	public class Main {
+		
+	    public static void main(String[] args) {
+	        int[][] sourceMatrix = new int[3][4];
+	        int[][] transposedMatrix = new int[4][3];
 
-    public static void main(String[] args) {
-        int [][] matrix = new int[3][3];
-        int x=0;
+	        sourceMatrixWithFiller(sourceMatrix);
+	        transposedMatrix(sourceMatrix, transposedMatrix);
+	    }
+/**
+ * Method for initial matrix
+ * @param sourceMatrix
+ * @return sourceMatrix
+ */
+	    public static int[][] sourceMatrixWithFiller(int[][] sourceMatrix) {
+	        int numberFiller = 1;
+	        System.out.println("Вихідна матриця:");
+	        for (int i = 0; i < sourceMatrix.length; i++) {
+	            for (int j = 0; j < sourceMatrix[i].length; j++) {
+	                sourceMatrix[i][j] = numberFiller++;
+	                System.out.print(sourceMatrix[i][j] + " ");
+	            }
+	            System.out.println();
+	        }
+	        return sourceMatrix; //return the result  
+	    }
+/**
+ * Method for transposing and displaying a new matrix
+ * @param sourceMatrix
+ * @param transposedMatrix
+ * @return transposedMatrix
+ */
+	    public static int[][] transposedMatrix(int[][] sourceMatrix, int[][] transposedMatrix) {
+	        for (int i = 0; i < sourceMatrix.length; i++) {
+	            for (int j = 0; j < sourceMatrix[i].length; j++) {
+	                transposedMatrix[j][i] = sourceMatrix[i][j];
+	            }
+	        }
+	        System.out.println("Транспонована матриця:");
+	        for (int i = 0; i < transposedMatrix.length; i++) {
+	            for (int j = 0; j < transposedMatrix[i].length; j++) {
+	                System.out.print(transposedMatrix[i][j] + " ");
+	            }
+	            System.out.println();
+	        }
+	        return transposedMatrix; //return the result  
+	    }
+	}
 
-        for(int[] row:matrix)
-            Arrays.fill(row,x);
-
-        for(int[] row:matrix)
-            System.out.println(Arrays.toString(row));
-    }
-}
